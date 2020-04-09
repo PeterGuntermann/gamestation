221c574a4b2778e67b2b58cc9984420ec633e9b8
Peter Guntermann <pguntermann@andrena.de>
2020-04-09 18:38:40 +0200

    First try towards a rest api

--- gamestation.py
+++ gamestation.py
@@ -1,1 +1,13 @@
-print('It works.')
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask
+app = Flask(__name__)
+
+print('GameStation is now running.')
+
+@app.route('/')
+def index():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+app.run()
