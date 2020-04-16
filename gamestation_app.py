0d81bb3d7cf8b5d02536fac03f1e237920a6ee34
Peter Guntermann <pguntermann@andrena.de>
2020-04-16 21:38:55 +0200

    Try using a template variable

--- gamestation_app.py
+++ gamestation_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("main_page.html")
+    return render_template("main_page.html", user="Fremder")
 
 
 if __name__ == '__main__':
